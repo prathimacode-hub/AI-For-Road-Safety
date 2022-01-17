597c64b3e2af2a09fbbfc17097f5300e2e774599
Prathima Kadari <kadariprathima4@gmail.com>
2022-01-17 22:38:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,175 @@
+import datetime
+import math
+import folium
+import geopandas as gpd
+import geopy
+import networkx as nx
+import joblib
+import osmnx as ox
+import shapely.wkt
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+import time
+import base64
+from branca.element import Figure
+from folium.features import DivIcon
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+from PIL import Image
+from streamlit_folium import folium_static
+from dateutil.relativedelta import relativedelta
+from data_collection import *
+from predict import *
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import time
+import geemap
+
+
+st.set_page_config(
+    page_title="AI For Road Safety In Hyderabad",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.sidebar.markdown('<h1 style="margin-left:8%; color:	#FF9933 ">AI For Road Safety In Hyderabad </h1>',
+                    unsafe_allow_html=True)
+
+add_selectbox = st.sidebar.radio(
+    "",
+    ("Home", "About", "Features", "Road Safety", "Visualizations", "Conclusion", "Team")
+)
+
+if add_selectbox == 'Home':
+    
+    LOGO_IMAGE = "omdena_india_logo.png"
+    
+    st.markdown(
+          """
+          <style>
+          .container {
+          display: flex;
+        }
+        .logo-text {
+             font-weight:700 !important;
+             font-size:50px !important;
+             color: #f9a01b !important;
+             padding-top: 75px !important;
+        }
+        .logo-img {
+             float:right;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown(
+          f"""
+          <div class="container">
+               <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+          </div>
+          """,
+          unsafe_allow_html=True
+    )
+    
+    st.subheader('PROBLEM STATEMENT')
+    
+    st.markdown('', 
+         unsafe_allow_html=True)
+
+    
+elif add_selectbox == 'About':
+    
+    st.subheader('ABOUT THE PROJECT')
+
+    st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    
+    st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+    st.markdown('• ',unsafe_allow_html=True)
+
+
+elif add_selectbox == 'Features':
+
+    st.subheader('FEATURES INVOLVED')
+
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+    
+    
+elif add_selectbox == 'Road Safety':   
+    
+    st.subheader('ROAD SAFETY')    
+    
+        
+elif add_selectbox == 'Output Visualizations':
+    
+    st.subheader('VISUALIZATIONS')
+
+   
+elif add_selectbox == 'Conclusion':
+
+    st.subheader('TECH STACK')
+
+    st.markdown('• Web Scraping - ', unsafe_allow_html=True)
+    st.markdown('• Data Wrangling & Pre-Processing - ', unsafe_allow_html=True)
+    st.markdown('• NLP Modelling - ', unsafe_allow_html=True) 
+    st.markdown('• Image Classification & Computer Vision Modelling - ', unsafe_allow_html=True) 
+    st.markdown('• Dashboard - Streamlit', unsafe_allow_html=True) 
+
+    st.subheader('PROJECT SUMMARY')
+
+    st.markdown('', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    
+    st.subheader('CONCLUSION')
+    
+    st.markdown('', unsafe_allow_html=True)
+    
+    
+elif add_selectbox == 'Team':
+    
+    st.subheader('COLLABORATORS')
+
+   
+    st.markdown('• <a href="">Nikhil Shrestha</a>',
+                unsafe_allow_html=True)
+      st.markdown('• <a href="">Bhushan Choughule</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="https://www.linkedin.com/in/tanisha-banik-04b511173/">Tanisha Banik</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="https://www.linkedin.com/in/prathima-kadari/">Prathima Kadari</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Shreya Chawla</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Hardik</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Tarun Goli</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Chittoor Vamsi</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Harshal Hirpara</a>',
+                unsafe_allow_html=True)
+    st.markdown('• <a href="">Pawan Roy</a>',
+                unsafe_allow_html=True)
+    
+
+    st.subheader('PROJECT MANAGER')
+
+    st.markdown('• <a href="">Yahya</a>', unsafe_allow_html=True)
+                
