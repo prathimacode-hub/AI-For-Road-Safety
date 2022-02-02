bb16e0f5d41d2b1f5351b0ae19cf9aaeb7b1d9c1
Prathima Kadari <kadariprathima4@gmail.com>
2022-02-02 16:17:25 +0530

    Update predict_sentiment.py

--- predict_sentiment.py
+++ predict_sentiment.py
@@ -27,12 +27,12 @@ def predict_sentiment(ab, data):
     preds = pd.DataFrame(preds)
     ab['Class'] = value
     for row in range(ab.shape[0]):
-        if salinity(ab.loc[row, 'sentiment']) == 'positive':
+        if sentiment_analysis(ab.loc[row, 'sentiment']) == 'positive':
             if preds.iloc[row, 0] == value:
                 ab.loc[row, 'Class'] = value
             else :
                 ab.loc[row, 'Class'] = preds.iloc[row, 0] 
-        elif salinity(ab.loc[row, 'sentiment']) == 'Neutral':
+        elif sentiment_analysis(ab.loc[row, 'sentiment']) == 'Neutral':
             if preds.iloc[row, 0] == value:
                 ab.loc[row,'Class'] = value
             else :
